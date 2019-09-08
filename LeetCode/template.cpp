4c9b3354781299922ccbc71468e1342dca43a76d
NewMai <610098201@qq.com>
2019-09-09 00:03:38 +0800

    Reorganize the code

--- LeetCode/template.cpp
+++ LeetCode/template.cpp
@@ -1,22 +1,39 @@
 
 #include "common.h"
 
+// https://leetcode-cn.com/problems/
+// 
 
 class Solution 
 {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) 
+	int test(vector<int>& height)
 	{
-		;
+		int ret = 0;
+		
+
+		return ret;
 	}
+
 };
 
 
 int main()
 {
-	
+	Solution sln;
+	int ret = 0;
+	string str = "test";
+	vector<int> height = { 1,8,6,2,5,4,8,3,7 };
+
+
+	ret = sln.test(height);
+	cout << ret << endl;
+
+
 	return 0;
 }
+
+
 
 
 
