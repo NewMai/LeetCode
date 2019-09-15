4c618c589d2620bd2c80986f0d7c052a9734b998
NewMai <610098201@qq.com>
2019-09-15 23:43:10 +0800

    AC LeetCode19-removeNthFromEnd for another method

--- LeetCode/middle/removeNthFromEnd.cpp
+++ LeetCode/middle/removeNthFromEnd.cpp
@@ -24,6 +24,48 @@ class Solution
 public:
 	ListNode* removeNthFromEnd(ListNode* head, int n)
 	{
+		struct SuperNode
+		{
+			ListNode* pre;
+			int idx;
+			SuperNode(ListNode* p, int i) :pre(p), idx(i) {}
+		};
+		ListNode* p = head;
+		int i = 0, j = 0;
+		vector<SuperNode> nodeArr;
+
+		if (head == NULL) return head;
+
+		SuperNode sn1(NULL, 0);
+		nodeArr.push_back(sn1);
+
+		i = 0;
+		while (p)
+		{
+			i += 1;
+
+			SuperNode sn2(p, i);
+			nodeArr.push_back(sn2);
+
+			p = p->next;
+		}
+
+		j = i - n;
+		if (j == 0)
+		{
+			// Delete the first one
+			head = head->next;
+		}
+		else
+		{
+			p = nodeArr[j].pre;
+			p->next = p->next->next;
+		}
+
+		return head;
+	}
+	ListNode* removeNthFromEnd_old(ListNode* head, int n)
+	{
 		// AC
 		ListNode* p = head, *pre = NULL;
 		int i = 0, j = 0;
@@ -36,7 +78,7 @@ public:
 			i += 1;
 			p = p->next;
 		}
-		
+
 		j = i - n;
 		p = head;
 		pre = NULL;
@@ -59,7 +101,6 @@ public:
 
 		return head;
 	}
-
 };
 
 ListNode* AddNode(ListNode* head, int x)
