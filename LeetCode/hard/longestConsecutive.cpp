09bfb2223dac18d880c352c6064ded39656c7482
NewMai <610098201@qq.com>
2019-09-24 17:11:58 +0800

    AC after reference LeetCode128-longestConsecutive()

--- LeetCode/hard/longestConsecutive.cpp
+++ LeetCode/hard/longestConsecutive.cpp
@@ -2,7 +2,7 @@
 #include "../common.h"
 
 // https://leetcode-cn.com/problems/longest-consecutive-sequence/
-// 
+// AC after reference
 
 class Solution 
 {
@@ -11,20 +11,21 @@ public:
 	{
 		int ret = 0;
 		int i = 0, j = 0;
-		map<int,  int> mp;
-		int size = 0;
+		int size = nums.size();
 		int t = 0;
 		int curN = 0;
 		int curL = 0;
 		int max = 0;
-		map<int, int>::iterator itmp;
+		unordered_map<int, int> mp;
+		unordered_map<int, int>::iterator itmp;
+	
 
-		for (i = 0; i < nums.size(); i++)
+		for (i = 0; i < size; i++)
 		{
 			mp[nums[i]] = 1;
 		}
 
-		for (i = 0; i < nums.size(); i++)
+		for (i = 0; i < size; i++)
 		{
 			curN = nums[i];
 			curL = 0;
@@ -35,7 +36,6 @@ public:
 				curN++;
 				curL++;
 				//t = mp[curN];
-
 				itmp = mp.find(curN);
 
 			} while (itmp != mp.end());
@@ -55,8 +55,8 @@ int main()
 	Solution sln;
 	int ret = 0;
 	string str = "test";
-	vector<int> nums = { 1,4,8,3,7 };
-
+	//vector<int> nums = { 1,4,8,3,7 };
+	vector<int> nums = { 100,4,200,1,3,2 };
 
 	ret = sln.longestConsecutive(nums);
 	cout << ret << endl;
